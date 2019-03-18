f6f9d961b02245650fd9fff7d4aec255ef671de8
Mouni Mullapudi <mullapudi24@gmail.com>
2019-03-18 12:53:19 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 class BankAccount
 
+  attr_reader :name
+  attr_accessor :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(monay)
+     self.balance = self.balance + monay
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    if self.balance > 0 && status == "open"
+      true
+    else
+      false
+  end
 end
+
+  def close_account
+    self.status = "closed"
+  end
+end 
