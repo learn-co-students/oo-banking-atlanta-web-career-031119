5a3df4d070cdd5fc0fa2478733314c5c45315351
github username <github email address>
2019-07-28 21:41:54 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,35 @@
 class Transfer
-  # your code here
+  def initialize(sender, receiver, amount)
+    @sender=sender
+    @receiver=receiver
+    @amount=amount
+    @status='pending'
+  end
+
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def valid?
+    self.sender.valid? && self.receiver.valid? && self.sender.balance-self.amount>=0
+  end
+
+  def execute_transaction
+    if self.valid? && self.status=='pending'
+      self.sender.balance-=self.amount
+      self.receiver.balance+=self.amount
+      self.status='complete'
+    else
+      self.status='rejected'
+      'Transaction rejected. Please check your account balance.'
+    end
+  end
+
+  def reverse_transfer
+    if self.status=='complete'
+      self.receiver.balance-=self.amount
+      self.sender.balance+=self.amount
+      self.status='reversed'
+    else
+      'Transfers that have not completed cannot be reversed.'
+    end
+  end
 end
